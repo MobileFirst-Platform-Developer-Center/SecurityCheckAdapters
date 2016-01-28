15df3b81ff6bc1a95a9da77b87f8d5578fde2860
Nathan Hazout <nathanh@il.ibm.com>
2016-01-28 14:29:24 +0200

    Send error message and remaining attemps in challenge

--- SecuritySampleWithAttempts/src/main/java/com/sample/SecuritySampleWithAttempts.java
+++ SecuritySampleWithAttempts/src/main/java/com/sample/SecuritySampleWithAttempts.java
@@ -2,12 +2,18 @@ package com.sample;
 
 import com.ibm.mfp.server.security.external.checks.impl.SecurityCheckWithAttempts;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by nathanh on 25/01/2016.
  */
 public class SecuritySampleWithAttempts extends SecurityCheckWithAttempts{
+
+    private transient String errorMsg = null;
+    public static final String USER_FIELD = "user";
+    public static final String PASSWORD_FIELD = "password";
+
     /**
      * Validate the credentials
      *
@@ -16,15 +22,21 @@ public class SecuritySampleWithAttempts extends SecurityCheckWithAttempts{
      */
     @Override
     protected boolean validateCredentials(Map<String, Object> credentials) {
-        if(credentials.containsKey("username") && credentials.containsKey("password")){
-            String username = credentials.get("username").toString();
-            String password = credentials.get("password").toString();
+        if(credentials.containsKey(USER_FIELD) && credentials.containsKey(PASSWORD_FIELD)){
+            String username = credentials.get(USER_FIELD).toString();
+            String password = credentials.get(PASSWORD_FIELD).toString();
 
             //For our example, credentials are valid if username == password
             if(username.equals(password)){
                 return true;
             }
+            else {
+                errorMsg = "username and password need to be identical";
+            }
 
+        }
+        else{
+            errorMsg = "username and password were not provided";
         }
 
         //In any other case, credentials are not valid
@@ -34,10 +46,14 @@ public class SecuritySampleWithAttempts extends SecurityCheckWithAttempts{
     /**
      * Create the challenge that should be sent to the client
      *
-     * @return the challenge object, may be a simple type (primitive or String), or a POJO/Map
+     * @return the challenge object, Map
      */
     @Override
     protected Map<String, Object> createChallenge() {
-        return null;
+        HashMap challenge = new HashMap();
+        challenge.put("errorMsg",errorMsg);
+        challenge.put("remainingAttempts",remainingAttempts);
+        return challenge;
+
     }
 }

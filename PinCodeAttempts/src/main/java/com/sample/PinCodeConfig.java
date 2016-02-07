d3f7e8698cfbde0d0cae08593f65d8aaf7696a6c
Nathan Hazout <nathanh@il.ibm.com>
2016-02-07 16:48:35 +0200

    Add error and warnings in PinCodeConfig

--- PinCodeAttempts/src/main/java/com/sample/PinCodeConfig.java
+++ PinCodeAttempts/src/main/java/com/sample/PinCodeConfig.java
@@ -12,8 +12,22 @@ public class PinCodeConfig extends SecurityCheckWithAttemptsConfig {
     public String pinCode;
 
     public PinCodeConfig(Properties properties) {
+        //Make sure to load the parent properties
         super(properties);
+
+        //Load the pinCode property
         pinCode = getStringProperty("pinCode", properties, "1234");
+
+        //Check that the PIN code is at least 4 characters long. Triggers an error.
+        if(pinCode.length() < 4){
+            addMessage(errors,"pinCode","pinCode needs to be at least 4 characters");
+        }
+
+        //Check that the PIN code is numeric. Triggers warning.
+        try
+        { int i = Integer.parseInt(pinCode); }
+        catch(NumberFormatException nfe)
+        { addMessage(warnings,"pinCode","PIN code contains non-numeric characters"); }
     }
 
 }

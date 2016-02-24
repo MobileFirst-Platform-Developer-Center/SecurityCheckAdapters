6e5cc087a989ad546d9ef8b365a047cb8819c7c5
Nathan Hazout <nathanh@il.ibm.com>
2016-02-24 15:00:00 +0200

    Cleanup

--- PinCodeAttempts/src/main/java/com/sample/PinCodeConfig.java
+++ PinCodeAttempts/src/main/java/com/sample/PinCodeConfig.java
@@ -19,15 +19,17 @@ public class PinCodeConfig extends CredentialsValidationSecurityCheckConfig {
         pinCode = getStringProperty("pinCode", properties, "1234");
 
         //Check that the PIN code is at least 4 characters long. Triggers an error.
-        if(pinCode.length() < 4){
+        if(pinCode.length() < 4) {
             addMessage(errors,"pinCode","pinCode needs to be at least 4 characters");
         }
 
         //Check that the PIN code is numeric. Triggers warning.
-        try
-        { int i = Integer.parseInt(pinCode); }
-        catch(NumberFormatException nfe)
-        { addMessage(warnings,"pinCode","PIN code contains non-numeric characters"); }
+        try {
+            int i = Integer.parseInt(pinCode);
+        }
+        catch(NumberFormatException nfe) {
+            addMessage(warnings,"pinCode","PIN code contains non-numeric characters");
+        }
     }
 
 }
